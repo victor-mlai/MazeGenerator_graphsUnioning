cc2873f9c592524e34eb4ebf2543c9acc4d4ea98
victorMunteanu <muntenau.victor0201@gmail.com>
2017-12-07 13:56:56 +0200

    BFS works

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,20 @@
 #include <time.h>	// wait function
 #include <stdlib.h>	// malloc and calloc
 #include <conio.h>	// _getch()
+#include <queue>	// used by bfs
 
-#define BLOCK 0
-#define N {0, -2}
-#define E {2, 0}
-#define W {-2, 0}
-#define S {0, 2}
+#define N 25	// number of lines
+#define M 21	// number of columns
+#define BLOCK -1
+#define MAXDIST 2*(N+M)
+#define UP1 {0, -1}
+#define RIGHT1 {1, 0}
+#define DOWN1 {0, 1}
+#define LEFT1 {-1, 0}
+#define UP2 {0, -2}
+#define RIGHT2 {2, 0}
+#define DOWN2 {0, 2}
+#define LEFT2 {-2, 0}
 
 struct vec2
 {
@@ -24,6 +32,14 @@ struct vec2
 		x = other.x;
 		y = other.y;
 	}
+
+	bool operator== (vec2& other) {
+		return (x == other.x) && (y == other.y);
+	}
+
+	bool operator!= (vec2& other) {
+		return (x != other.x) || (y != other.y);
+	}
 };
 
 void wait(float seconds)
@@ -32,22 +48,22 @@ void wait(float seconds)
 	while (clock() < endwait) {}
 }
 
-void afisare(int **mat, int height, int width)
+void display(int **mat)
 {
 	int i, j;
-	int zid = 219;
+	int block = 219;
 
 	printf("\n");
-	for (j = 1; j <= width; j++)
+	for (j = 0; j <= M + 1; j++)
 		printf(" %-2d", j);
 	printf("\n");
-	for (i = 1; i <= height; i++)
+	for (i = 0; i <= N + 1; i++)
 	{
-		for (j = 1; j <= width; j++)
+		for (j = 0; j <= M + 1; j++)
 			switch (mat[i][j])
 			{
 			case BLOCK: 
-				printf("%c%c%c", zid, zid, zid); 
+				printf("%c%c%c", block, block, block); 
 				break;
 			//case 1: printf("   "); break;
 			//case 2: printf(" v "); break;
@@ -55,7 +71,7 @@ void afisare(int **mat, int height, int width)
 			//case 4: printf(" ^ "); break;
 			//case 5: printf(" > "); break;
 			default:
-				printf("   ");
+				printf("%-3d", mat[i][j]);
 				break;
 			}
 		printf(" %d\n", i);
@@ -64,114 +80,150 @@ void afisare(int **mat, int height, int width)
 }
 
 void solveBFS(int** maze, vec2 start, vec2 exit) {
+	for (int i = 2; i < N; i += 2) {
+		for (int j = 2; j < M; j += 2) {
+			maze[i][j] = MAXDIST;
+		}
+	}
 
+	system("cls");
+	display(maze);
+
+	std::queue<vec2> q;
+	vec2 d[] = { UP1, RIGHT1, DOWN1, LEFT1 };	// directions
+
+	q.push(exit);
+
+	vec2 F, V;
+	while (!q.empty()) {
+		F = q.front();
+		q.pop();
+
+		
+
+		for (vec2 dir : d) {	// for each neighbour
+			V = F + dir;
+
+			if (maze[V.x][V.y] > maze[F.x][F.y] + 1) {
+				maze[V.x][V.y] = maze[F.x][F.y] + 1;
+				if (V == start)
+					break;
+				q.push(V);
+			}
+		}
+
+		if (V == start)
+			break;
+
+		system("cls");
+		display(maze);
+	}
+
+	system("cls");
+	display(maze);
 }
 
-// returneaza true cu o probabilitate de 1/nr
+// returns true with a probability of 1/nr
 bool cond(int nr) {
 	return ((float)rand() / RAND_MAX < (float)1 / nr);
 }
 
-int main()
-{
-	int i, j, n, m, r;
-	int id_graph, id_ng; 
-	int nrOfGraphs; // nr grafuri
+void createMaze(int** maze) {
+	int i, j;
+	int id_graph; // current graph id
+	int id_ng;	// new graph id
+	int nrOfGraphs;
 	int rx, ry;
 	int k;
-	vec2 d[5] = { N, E, S, W };	// directions
-	srand((unsigned int)time(NULL));
+	vec2 d[] = { UP2, RIGHT2, DOWN2, LEFT2 };	// directions
 
-	n = 17;	// number of lines
-	m = 15; // number of columns
-
-	int** maze = (int**)malloc((n + 2) * sizeof(int*));
-	for (i = 0; i < n + 2; i++)
-		maze[i] = (int*)calloc((m + 2), sizeof(int));
-
-	// creare labirint: conex
-	while (true) {
-		id_graph = 0;
-		for (i = 2; i < n; i += 2) {
-			for (j = 2; j < m; j += 2) {
-				id_graph++;
-				maze[i][j] = id_graph;
-			}
+	for (int i = 0; i < N + 2; i++) {
+		for (int j = 0; j < M + 2; j++) {
+			maze[i][j] = BLOCK;
 		}
+	}
 
-		afisare(maze, n, m);
-		nrOfGraphs = id_graph; // nr grafuri
+	id_graph = 0;
+	for (i = 2; i < N; i += 2) {
+		for (j = 2; j < M; j += 2) {
+			id_graph++;
+			maze[i][j] = id_graph;
+		}
+	}
 
-		vec2 x, y;
-		int nr;
-		while (nrOfGraphs > 1) {
-			// choose a random graph
-			rx = (((rand()) % (n - 4)) / 2) * 2 + 2;	// gives a random odd number between 2 and n - 2
-			ry = (((rand()) % (m - 4)) / 2) * 2 + 2;
-			id_graph = maze[rx][ry];
-			nr = 1;
-			for (i = 2; i < n; i += 2) {
-				for (j = 2; j < m; j += 2) {
-					if (maze[i][j] == id_graph) {
-						for (k = 0; k < 4; k++) {	// for each neighbour
-							if (maze[i + d[k].x][j + d[k].y] != 0 &&
-								maze[i + d[k].x][j + d[k].y] != id_graph) {
-								if (cond(nr)) {
-									x.x = i + d[k].x;
-									x.y = j + d[k].y;
-									y.x = i;
-									y.y = j;
-									nr++;
-								}
+	display(maze);
+	nrOfGraphs = id_graph; // nr grafuri
+
+	vec2 x, y;	// create connection between x and y (delete the block betwwen them)
+	int nr;
+	while (nrOfGraphs > 1) {
+		rx = (((rand()) % (N - 4)) / 2) * 2 + 2;	// gives a random odd number between 2 and n - 2
+		ry = (((rand()) % (M - 4)) / 2) * 2 + 2;
+		id_graph = maze[rx][ry];	// choose a random graph
+		nr = 1;
+		for (i = 2; i < N; i += 2) {
+			for (j = 2; j < M; j += 2) {
+				if (maze[i][j] == id_graph) {
+					for (vec2 dir : d) {	// for each neighbour
+						if (maze[i + dir.x][j + dir.y] != BLOCK &&
+							maze[i + dir.x][j + dir.y] != id_graph) {
+							if (cond(nr)) {
+								x = vec2(i + dir.x, j + dir.y);
+								y = vec2(i, j);
+								nr++;
 							}
 						}
 					}
 				}
 			}
-
-			maze[(x.x + y.x) / 2][(x.y + y.y) / 2] = 1;
-
-			id_ng = maze[x.x][x.y];
-
-			for (i = 2; i < n; i += 2) {
-				for (j = 2; j < m; j += 2) {
-					if (maze[i][j] == id_graph) {
-						maze[i][j] = id_ng;
-					}
-				}
-			}
-
-			system("cls");
-			afisare(maze, n, m);
-			//wait((float)0.1);
-			nrOfGraphs--;
 		}
 
-		maze[n - 1][1] = 1;
-		maze[2][m] = 1;
+		id_ng = maze[x.x][x.y];
+
+		maze[(x.x + y.x) / 2][(x.y + y.y) / 2] = MAXDIST;
+
+		for (i = 2; i < N; i += 2) {
+			for (j = 2; j < M; j += 2) {
+				if (maze[i][j] == id_graph) {
+					maze[i][j] = id_ng;
+				}
+			}
+		}
+
+		//system("cls");
+		//display(maze);
+		//wait(0.3f);
+		nrOfGraphs--;
+	}
+
+	maze[N - 1][1] = MAXDIST;
+	maze[2][M] = 0;
+}
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+
+	int** maze = (int**)malloc((N + 2) * sizeof(int*));
+	for (int i = 0; i < N + 2; i++)
+		maze[i] = (int*)malloc((M + 2) * sizeof(int));
+
+	while (true) {
+		createMaze(maze);
 
 		system("cls");
-		afisare(maze, n, m);
+		display(maze);
 
-		//printf("\n Afisati solutie?\nd\\n\n");
-		//if (_getch() == 'n')
-		//	return 0;
+		printf("\n Afisati solutie?\nd\\n\n");
+		if (_getch() == 'd')
+			solveBFS(maze, vec2(N - 1, 1), vec2(2, M));
 	
 		printf("\n Display another maze?\nd\\n\n");
 		if (_getch() == 'n')
 			break;
-
-		for (int i = 0; i < n + 2; i++) {
-			for (int j = 0; j < m + 2; j++) {
-				maze[i][j] = 0;
-			}
-		}
 	}
-	
 
-	/* rezolvare labirint */
-	solveBFS(maze, vec2(n - 1, 1), vec2(2, m));
-
+	printf("\n Press any key to exit\n");
 	_getch();	// waits any key
 
 	return 0;
